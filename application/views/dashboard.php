28b68ef093babeefa739a4e86743f88524e96a52
itkenbdoob <vasquezkennethb@ymail.com>
2014-10-03 17:02:58 +0800

    Realignment of body container

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -3,9 +3,9 @@
 		<div class='row col-lg-3' id="ml-body">
 			<h1>Hello World!</h1>
 			<ul>
-				<h4><a href="<?php echo base_url('students/unpaid')?>">UNPAID</a></h4>
-				<h4><a href="<?php echo base_url('students/balance')?>">WITH BALANCE</a></h4>
-				<h4><a href="<?php echo base_url('students/summary')?>">SUMMARY</a></h4>
+				
+				
+				
 			</ul>
 		</div>
 		<div class="row col-lg-6 col-lg-offset-4" id="mr-body">
@@ -13,11 +13,16 @@
 		</div>
 	</div> -->
 	<div class="row">
-		<div class="col-lg-3">
-			asdasdasdd
+		<div class="col-sm-3 col-sm-offset-1" id="one">
+			<h1>HELLO WORLD!</h1>
+			<ul>
+				<li><h3><a href="<?php echo base_url('students/unpaid')?>">UNPAID</a></h3></li>
+				<li><h3><a href="<?php echo base_url('students/balance')?>">WITH BALANCE</a></h3></li>
+				<li><h3><a href="<?php echo base_url('students/summary')?>">SUMMARY</a></h3></li>
+			</ul>
 		</div>
-		<div class="col-lg-9">
-			asdasdads
+		<div class="col-sm-6" id="two">
+			<h1>HELLO WORLD!</h1>
 		</div>
 	</div>
 <?php $this->load->view('includes/footer'); ?>
